9f36429a6601656f8c6d91cd9c47218817d1ab09
Bao <bthuynh90@gmail.com>
2021-01-10 17:10:45 -0500

    added files modified:   main.py

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,1 +1,17 @@
+import os
+import csv
+
+
+pybank_csv = os.path.join("Resources", "budget_data.csv")
+
+with open(pybank_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    print(csvreader)
+
+
+#print("Financial Analysis")
+#print("---------------------------")
+
+
 
